e67654019ea0592f244c74095e361743d5caf0e6
Shu-YuL <slin1@ualberta.ca>
2023-01-23 22:35:00 -0700

    integrate wifi and oled code, server code ver.0.5

---
+++ Firmware/Base_Station/main/server_include.h
@@ -0,0 +1,68 @@
+#ifndef SERVER_INCLUDE_H
+#define SERVER_INCLUDE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> //Requires by memset
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+#include <esp_http_server.h>
+
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "freertos/event_groups.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_netif.h"
+#include "driver/gpio.h"
+#include <lwip/sockets.h>
+#include <lwip/sys.h>
+#include <lwip/api.h>
+#include <lwip/netdb.h>
+
+#include "wifi_include.h"
+
+#define MIN(a, b) (((a) < (b)) ? (a) : (b))
+
+#define MAX_INPUTS 10
+#define INPUT_LENGTH 50
+#define ID_LENGTH 50
+
+#define LED_PIN 2
+
+#define EXAMPLE_ESP_WIFI_SSID WIFI_SSID
+#define EXAMPLE_ESP_WIFI_PASS WIFI_PASSWORD
+#define EXAMPLE_ESP_MAXIMUM_RETRY WIFI_MAX_TRIES
+
+/* The event group allows multiple bits for each event, but we only care about two events:
+ * - we are connected to the AP with an IP
+ * - we failed to connect after the maximum amount of retries */
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT BIT1
+
+/* function prototypes */
+esp_err_t send_web_page(httpd_req_t *req);
+
+esp_err_t Web_main_handler(httpd_req_t *req);
+esp_err_t input_post_handler(httpd_req_t *req);
+esp_err_t delete_post_handler(httpd_req_t *req);
+esp_err_t add_post_handler(httpd_req_t *req);
+
+httpd_handle_t setup_server(void);
+void stop_webserver(httpd_handle_t server);
+void restart_webserver(httpd_handle_t server);
+
+/* Global variables */
+extern httpd_uri_t uri_get;
+extern httpd_uri_t input_post_uri;
+extern httpd_uri_t delete_uri;
+extern httpd_uri_t add_uri;
+
+
+extern char main_resp[];
+extern int led_state;
+extern char Web_state[];
+
+#endif
---
+++ Firmware/Base_Station/main/wifi_functions.c
@@ -0,0 +1,79 @@
+#include "wifi_include.h"
+
+static const char *TAG = "WIFI";
+
+/***** WIFI Statics *****/
+void setup_led(void) {
+    ESP_LOGI(TAG, "LED on GPIO %d set to indicate successful Wi-Fi connection", LED_GPIO);
+    gpio_reset_pin(LED_GPIO);
+    gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
+}
+
+void setup_nvs(void) {
+    //Initializes NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+}
+
+void setup_wifi(void) {
+    //to setup ESP32 as a station (STA) to a simple non-WPA2 access point (AP) like home Wi-Fi 
+
+    wifi_init_config_t init_config = WIFI_INIT_CONFIG_DEFAULT(); 
+    //ESP_ERROR_CHECK() checks the esp_err_t return value of API functions, and prints error message if function doesn't return ESP_OK and aborts
+    //to avoid aborting, use ESP_ERROR_CHECK_WITHOUT_ABORT()
+    //see Espressif's Error Handling Doc: "Many ESP-IDF examples use ESP_ERROR_CHECK to handle errors from various APIs. This is not the best practice for applications, and is done to make example code more concise." May be simpler to just check return values manually
+    ESP_ERROR_CHECK(esp_wifi_init(&init_config)); //must be called first
+    
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA)); //set ESP32 to be a station (STA); to set as AP, use WIFI_MODE_AP
+
+    //wifi_config_t wifi_configuration;
+    //wifi_configuration.sta = {.ssid = WIFI_SSID , .password = WIFI_PASSWORD, .threshold.authmode = WIFI_AUTHORIZATION_MODE_THRESHOLD, .sae_pwe_h2e = WPA3_SAE_PWE_BOTH,};
+
+
+    wifi_config_t wifi_configuration = {
+        .sta = {.ssid = WIFI_SSID, .password = WIFI_PASSWORD,
+            /* Setting a password implies station will connect to all security modes including WEP/WPA.
+             * However these modes are deprecated and not advisable to be used. Incase your Access point
+             * doesn't support WPA2, these mode can be enabled by commenting below line */
+	     .threshold.authmode = WIFI_AUTH_OPEN, .sae_pwe_h2e = WPA3_SAE_PWE_BOTH,
+         },
+    };
+
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_configuration));
+
+    ESP_ERROR_CHECK(esp_wifi_start());
+    
+    return;
+}
+
+// esp_err_t connect_wifi(void) {
+//     //attempts to connect ESP32 to a WiFi Access Point specified by setup_wifi()
+    
+//     esp_err_t wifi_connection_status = !ESP_OK;
+//     uint8_t tries = 0;
+
+//     while (wifi_connection_status != ESP_OK && tries < WIFI_MAX_TRIES ) {  //attempt to connect to AP until connection established, or max attempts reached
+//         wifi_connection_status = esp_wifi_connect();
+//         //vTaskDelay(2000 / portTICK_PERIOD_MS); //wait 2s before retrying to connect to AP
+//         tries++;
+//     }
+
+
+//     if (wifi_connection_status == ESP_OK) {
+//         ESP_LOGI(TAG, "WiFi Successfully Connected!");
+//         wifi_ap_record_t ap_info;
+//         esp_wifi_sta_get_ap_info(&ap_info);
+//         ESP_LOGI(TAG, "__________________________________________");
+//         ESP_LOGI(TAG, "AP SSID: %s, AP MAC Address: %s, rssi: %d ", ap_info.ssid, ap_info.bssid, ap_info.rssi);
+//         gpio_set_level(LED_GPIO, 1);
+//     }
+//     else {ESP_LOGI(TAG, "WiFi Failed to Connected");
+//     gpio_set_level(LED_GPIO, 0);
+//     }
+    
+//     return wifi_connection_status;
+// }
